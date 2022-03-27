d0180ab43a175f0212c4cb32ec1803db85ff4186
msgimanila <msgimanila@yahoo.com>
2022-03-27 19:38:43 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
-import Books from '././Books.js';
+ 
 function App() {
  
   return (
