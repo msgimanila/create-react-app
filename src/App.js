e22f233a150dfd717a4003cd8d7a6ee8479eca64
msgimanila <msgimanila@yahoo.com>
2022-04-10 14:32:50 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
 import './App.css';
  
 function App() {
@@ -8,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
